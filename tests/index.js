05380feed6b7346f04408c22ba3ed704cc12c43e
Gustavo Henrique <gu.bat98@gmail.com>
2017-07-05 23:40:16 -0300

    WikiPedia Example

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,29 @@
 const tweetVerse = require('./../app');
 
-tweetVerse.crawl({
-    config: {
-        consumer_key: '?',
-        consumer_secret: '?',
-        access_token_key: '?',
-        access_token_secret: '?'
-    },
-    uri: 'https://noticias.uol.com.br/',
-    selector: '.manchete-sem-foto-3colunas h1',
-    cb: function(title) {
-        tweetVerse.tweet({
-            body: 'tweetVerse: "' + title + '"',
-            config: this.config
-        });
-    }
-});
+setInterval(function() {
+    tweetVerse.crawl({
+        config: {
+            consumer_key: '?',
+            consumer_secret: '?',
+            access_token_key: '?',
+            access_token_secret: '?'
+        },
+        uri: 'https://pt.wikipedia.org/wiki/Especial:Aleat%C3%B3ria',
+        selector: '#mw-content-text p:nth-child(2)',
+        cb: function(title) {
+            // tweetVerse.tweet({
+            //     body: 'tweetVerse: "' + title + '"',
+            //     config: this.config
+            // });
+            console.log(title);
+            if (title != '') {
+                if (title.length <= 140) {
+                    tweetVerse.tweet({
+                        body: title,
+                        config: this.config
+                    });
+                }
+            }
+        }
+    });
+}, 1000)
